3939789221684c43941dcb54948a817fe57d169c
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-07-12 21:24:21 -0400

    Time: 84 ms (23.32%), Space: 17.8 MB (23.94%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,5 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # lowest = prices[0]
-        # current_buy = None
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         profit = 0
 
         for i in range(len(prices) - 1):
