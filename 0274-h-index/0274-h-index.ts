934404d8de3ecbbd98a0990ee8e13444ed152673
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-08-30 04:56:12 -0400

    Time: 70 ms (27.85%), Space: 43.8 MB (88.6%) - LeetHub

---
+++ 0274-h-index/0274-h-index.ts
@@ -0,0 +1,15 @@
+function hIndex(citations: number[]): number {
+    if (citations.length === 1 && citations[0] == 0) return 0;
+    let h: number = 0;
+    citations.sort((a, b) => b - a);
+    for (let i:number = 0; i < citations.length; i ++) {
+        if (citations[i] >= i + 1) {
+            h = i + 1
+        } else {
+            break;
+        }
+    };
+
+    return h;
+    
+};
