6ebd8188522c380fe65eada49ce89ad35bd5eada
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-13 19:19:29 -0400

    Time: 54 ms (97.26%), Space: 46.5 MB (38.81%) - LeetHub

---
+++ 0135-candy/0135-candy.ts
@@ -0,0 +1,20 @@
+function candy(ratings: number[]): number {
+    // Have two trackers, for i and i+1
+    // create a new array, initialized at 0
+    // Iterate through the array
+    // increment based on need
+
+    const res: number[] = new Array(ratings.length).fill(1);
+    for (let i: number = 0; i < ratings.length - 1; i ++) {
+        if (ratings[i + 1] > ratings[i] && res[i] >= res[i + 1]) {
+            res[i + 1] += res[i];
+        }
+    }
+    for (let i: number = ratings.length - 1; i > 0; i --) {
+        if (ratings[i - 1] > ratings[i] && res[i - 1] <= res[i]) {
+            res[i - 1] = res[i] + 1;
+        }
+    }
+    console.log(res)
+    return res.reduce((accum, val) => accum += val, 0);
+};
