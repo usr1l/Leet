c73b69b442b92f58d7d240df34aa195b8e339672
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-19 02:17:40 -0400

    Time: 56 ms (56.54%), Space: 43.1 MB (33.55%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,11 +1,10 @@
 function strStr(haystack: string, needle: string): number {
-    const needleLength: number = needle.length;
 
     // loop through until haystack left is less than needleLength
-    for (let i: number = 0; i <= haystack.length - needleLength; i ++) {
+    for (let i: number = 0; i <= haystack.length - needle.length; i ++) {
 
         // check using slice
-        if (haystack.slice(i, needleLength + i) === needle) {
+        if (haystack.slice(i, needle.length + i) === needle) {
             return i;
         };
     };
