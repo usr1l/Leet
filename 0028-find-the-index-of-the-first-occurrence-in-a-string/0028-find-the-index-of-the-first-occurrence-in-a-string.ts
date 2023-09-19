cdd3f1c097589ec04b5820c0bc853f576576d9f0
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-19 02:16:54 -0400

    Time: 88 ms (5.38%), Space: 47.2 MB (5.01%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,14 @@
+function strStr(haystack: string, needle: string): number {
+    const needleLength: number = needle.length;
+
+    // loop through until haystack left is less than needleLength
+    for (let i: number = 0; i <= haystack.length - needleLength; i ++) {
+
+        // check using slice
+        console.log(haystack.slice(i, needleLength + i))
+        if (haystack.slice(i, needleLength + i) === needle) {
+            return i;
+        };
+    };
+    return -1;
+};
