c12d31fcaa9b253df307f90baa4178e873b6eda0
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-21 22:58:24 -0400

    Time: 54 ms (95.64%), Space: 46.6 MB (33.93%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+        // let res = [];
+
+        
+        // for (let i = 0; i < 9; i++) {
+        //     for (let j = 0; j < 9; j++) {
+        //         let element = board[i][j];
+        //         if (element !== '.') {
+        //             res.push([i, element], [element, j], [Math.floor(i / 3), Math.floor(j / 3), element]);
+        //         }
+        //     }
+        // }
+
+        // return res.length === new Set(res.map( val => JSON.stringify(val))).size;
+        let res = []; 
+
+
+        // loop through the puzzle and create a res array to push each element into the array, if each element is unique, every push should push unique elements
+        for (let i = 0; i < 9; i ++) {
+            for (let j = 0; j < 9; j ++) {
+                const currElement = board[i][j];
+                if (currElement !== '.') {
+
+                    // we want to swap positions of currElement here for false duplicates, i.e. in condition where i === j
+                    res.push([currElement, i], [j, currElement], [Math.floor(i / 3), Math.floor(j / 3), currElement]);
+                };
+            };
+        };
+
+        // use set to get rid of duplicates, if every item is unique, length is equal, and sudoku is valid
+        return res.length === new Set(res.map(val => JSON.stringify(val))).size;
+};
