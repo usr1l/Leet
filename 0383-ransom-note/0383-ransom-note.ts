0a2fe9fbd7686987307142b20acd7e77a157c2be
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-24 02:24:27 -0400

    Time: 82 ms (44.58%), Space: 46.5 MB (46.85%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.ts
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -1,8 +1,13 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    const passed: {} = {};
-    for (const letter of ransomNote) passed[letter] ? passed[letter]++ : passed[letter] = 1;
-    for (const letter of magazine) passed[letter] ? passed[letter] > 1 ? passed[letter]-- : delete passed[letter] : null;
+    // const passed: {} = {};
+    // for (const letter of ransomNote) passed[letter] ? passed[letter]++ : passed[letter] = 1;
+    // for (const letter of magazine) passed[letter] ? passed[letter] > 1 ? passed[letter]-- : delete passed[letter] : null;
 
-    return !Object.keys(passed).length;
+    // return !Object.keys(passed).length;
 
+    for (const letter of magazine) {
+        ransomNote = ransomNote.replace(letter, '');
+    };
+
+    return !ransomNote.length;
 };
