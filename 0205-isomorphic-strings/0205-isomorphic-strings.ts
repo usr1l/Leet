2ad98ebf39fde5c5b6e0c70bdba2e1aaa025babc
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-24 14:30:37 -0400

    Time: 67 ms (57.67%), Space: 45.8 MB (26.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.ts
+++ 0205-isomorphic-strings/0205-isomorphic-strings.ts
@@ -1,15 +1,15 @@
 function isIsomorphic(s: string, t: string): boolean {
     const sMatches: {} = {};
     const tMatches: {} = {};
-    for (let i = 0; i < s.length; i++) {
-        if (!sMatches[s[i]]) sMatches[s[i]] = t[i]
-        else if (sMatches[s[i]] !== t[i]) return false;
+    for (let i = 0; i < s.length; i ++) {
+        if ((s[i] in sMatches && sMatches[s[i]] !== t[i]) || (t[i] in tMatches && tMatches[t[i]] !== s[i])) {
+            return false;
+        } else {
+            sMatches[s[i]] = t[i];
+            tMatches[t[i]] = s[i];
+        };
     };
-    for (let i = 0; i < t.length; i++) {
-        if (!tMatches[t[i]]) tMatches[t[i]] = s[i]
-        else if (tMatches[t[i]] !== s[i]) return false;
-    }
-    return Object.keys(sMatches).length === Object.keys(tMatches).length;
+    return true;
 };
 
      
