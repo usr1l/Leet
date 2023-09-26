52e878fb752db377cbe23b7f4871db366376e671
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-26 19:33:31 -0400

    Time: 60 ms (46.15%), Space: 44.3 MB (50.94%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -6,13 +6,10 @@ var simplifyPath = function(path) {
     const stack = [];
     const dirs = path.split('/');
     for (const dir of dirs) {
-        if (dir === '.' || !dir) {
-            continue;
-        } else if (dir === '..') {
-            if (stack.length > 0) {
-                stack.pop();
-            }; 
+        if (dir === '.' || !dir) continue;
+        else if (dir === '..') {
+            if (stack.length > 0) stack.pop();
         } else stack.push(dir);
-    };
+    };  
     return "/" + stack.join('/');
 };
