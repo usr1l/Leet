1827229512149a02c4a06bf2cbd33955cf3e0cd0
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-09-29 13:32:38 -0700

    Time: 62 ms (41.67%), Space: 44.8 MB (26.14%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */ 
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) return true;
+
+    function mirrorTree(left, right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if (left.val !== right.val) return false;
+
+        return mirrorTree(left.left, right.right) && mirrorTree(left.right, right.left);
+    };
+
+    return mirrorTree(root.left, root.right);
+    
+};
