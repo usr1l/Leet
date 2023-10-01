3da80b2c98340f8ba2d340a4aac126e342021b08
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-01 09:36:51 -0700

    Time: 53 ms (66.39%), Space: 43.8 MB (60.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,24 +3,26 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    const res = [];
-    let currWord = '';
-    for (let i = 0; i < s.length; i++) {
-        // console.log(s[i] === ' ', s[i], currWord.length, currWord.length > 0)
-        if (s[i] === ' ' && currWord) {
-            // console.log(currWord);
-            res.unshift(currWord);
-            currWord = '';
+    // const res = [];
+    // let currWord = '';
+    // for (let i = 0; i < s.length; i++) {
+    //     // console.log(s[i] === ' ', s[i], currWord.length, currWord.length > 0)
+    //     if (s[i] === ' ' && currWord) {
+    //         // console.log(currWord);
+    //         res.unshift(currWord);
+    //         currWord = '';
 
-        } else if (s[i] === ' ') {
-            continue;
-        }
-        else if (s[i] && s[i] !== ' ') {
-            currWord += s[i];
-        };
-    };
+    //     } else if (s[i] === ' ') {
+    //         continue;
+    //     }
+    //     else if (s[i] && s[i] !== ' ') {
+    //         currWord += s[i];
+    //     };
+    // };
 
-    currWord ? res.unshift(currWord) : null;
+    // currWord ? res.unshift(currWord) : null;
 
-    return res.join(' ');
+    // return res.join(' ');
+
+    return s.split(' ').filter(word => word).reverse().join(' ');
 };
