06d9a36e37962bd88f5ca3605fe3b32b0f831f2d
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-01 09:36:15 -0700

    Time: 97 ms (5.23%), Space: 47.6 MB (5.2%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const res = [];
+    let currWord = '';
+    for (let i = 0; i < s.length; i++) {
+        // console.log(s[i] === ' ', s[i], currWord.length, currWord.length > 0)
+        if (s[i] === ' ' && currWord) {
+            console.log(currWord);
+            res.unshift(currWord);
+            currWord = '';
+
+        } else if (s[i] === ' ') {
+            continue;
+        }
+        else if (s[i] && s[i] !== ' ') {
+            currWord += s[i];
+        };
+    };
+
+    currWord ? res.unshift(currWord) : null;
+
+    return res.join(' ');
+};
