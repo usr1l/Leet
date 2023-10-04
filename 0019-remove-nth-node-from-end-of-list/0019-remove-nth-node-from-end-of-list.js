0e972526ef055425c3cde2544ff95e861fb77b82
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-04 15:17:54 -0700

    Time: 61 ms (19.05%), Space: 43.3 MB (36.44%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -12,28 +12,6 @@
  */
 var removeNthFromEnd = function(head, n) {
     if (!head) return;
-
-    // // use a pointer, because sometimes head can get removed
-    // let pointer = new ListNode(0, head);
-    // let curr = head;
-
-    // // map the whole list
-    // let map = new Map();
-    // map[`-1`] = pointer;
-    // let number = 0;
-    // while (curr) {
-    //     map[number] = curr;
-    //     curr = curr.next; 
-    //     number++;
-    // };
-
-    // // index of the node and the one before it
-    // let index = Object.keys(map).length - n;
-    // const node1 = map[`${index - 2}`];
-    // const node2 = map[`${index}`];
-    // node1.next = node2 || null;
-
-    // return pointer.next;
     let count = 1;
     const pointer = new ListNode(0, head);
     let pointer1 = pointer;
@@ -50,7 +28,6 @@ var removeNthFromEnd = function(head, n) {
         };
 
     };
-    console.log(pointer1, pointer2)
     pointer1.next = pointer2?.next || null;
 
     return pointer.next;
