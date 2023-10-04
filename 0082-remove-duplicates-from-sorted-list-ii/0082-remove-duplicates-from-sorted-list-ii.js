964d761f491681e9f893e859902d8b726151c486
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-04 15:41:47 -0700

    Time: 55 ms (71.92%), Space: 44.8 MB (9.54%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -16,16 +16,23 @@ var deleteDuplicates = function(head) {
     let pointer1 = pointer.next;
     let pointer2 = pointer1.next;
 
+    // while both pointers are valid
     while (pointer1 && pointer2) {
+        // check for duplicate values
         if (pointer2.val !== pointer1.val) {
             prev = prev.next;
             pointer2 = pointer2.next;
             pointer1 = pointer1.next;
         } else {
+            // if duplicate, keep goin until not
             while (pointer1.val === pointer2.val) {
                 pointer2 = pointer2.next;
+
+                // or break if end of list
                 if (!pointer2) break;
             }; 
+
+                // repath the nodes
                 const nextPointer = pointer2;
                 pointer1 = nextPointer;
                 prev.next = pointer1 || null;                
