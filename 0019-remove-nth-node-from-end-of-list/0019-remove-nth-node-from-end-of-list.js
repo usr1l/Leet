ebea7305952b72c1c81778632d73bb7d593c02a0
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-04 15:17:36 -0700

    Time: 58 ms (33.51%), Space: 44.3 MB (5.57%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -13,25 +13,45 @@
 var removeNthFromEnd = function(head, n) {
     if (!head) return;
 
-    // use a pointer, because sometimes head can get removed
-    let pointer = new ListNode(0, head);
-    let curr = head;
+    // // use a pointer, because sometimes head can get removed
+    // let pointer = new ListNode(0, head);
+    // let curr = head;
 
-    // map the whole list
-    let map = new Map();
-    map[`-1`] = pointer;
-    let number = 0;
-    while (curr) {
-        map[number] = curr;
-        curr = curr.next; 
-        number++;
+    // // map the whole list
+    // let map = new Map();
+    // map[`-1`] = pointer;
+    // let number = 0;
+    // while (curr) {
+    //     map[number] = curr;
+    //     curr = curr.next; 
+    //     number++;
+    // };
+
+    // // index of the node and the one before it
+    // let index = Object.keys(map).length - n;
+    // const node1 = map[`${index - 2}`];
+    // const node2 = map[`${index}`];
+    // node1.next = node2 || null;
+
+    // return pointer.next;
+    let count = 1;
+    const pointer = new ListNode(0, head);
+    let pointer1 = pointer;
+    let pointer2 = head;
+    let endPointer = head;
+
+    while (endPointer) {
+        endPointer = endPointer.next;
+        count++;
+        
+        if (count >= n + 2) {
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+        };
+
     };
-
-    // index of the node and the one before it
-    let index = Object.keys(map).length - n;
-    const node1 = map[`${index - 2}`];
-    const node2 = map[`${index}`];
-    node1.next = node2 || null;
+    console.log(pointer1, pointer2)
+    pointer1.next = pointer2?.next || null;
 
     return pointer.next;
 
