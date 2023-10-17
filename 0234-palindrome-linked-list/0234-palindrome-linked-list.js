5c7233cbed325e8707b0fb3c3f985e23cdd5dde9
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-17 10:45:29 -0700

    Time: 114 ms (54.85%), Space: 89.2 MB (10.33%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    const res = [];
+
+    while (head) {
+        res.push(head.val);
+        head = head.next;
+    };
+
+    return res.join('') === res.reverse().join('');
+};
