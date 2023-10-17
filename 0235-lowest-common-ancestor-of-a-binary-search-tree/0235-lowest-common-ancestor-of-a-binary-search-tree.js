9180d21dbcd5c0596d0e54f87ac117e0654b23db
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2023-10-17 11:00:25 -0700

    Time: 75 ms (38.96%), Space: 51.9 MB (78.86%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -15,10 +15,10 @@
 var lowestCommonAncestor = function(root, p, q) {
     // loop through, check for positioning of root val
     while (root) {
-        // if val is greater than both p and q, move to right
+        // if val is less than both p and q, move to right
         if (root.val < p.val && root.val < q.val) root = root.right;
         
-        // if val is less than both, move left
+        // if val is lgreater than both, move left
         else if (root.val > p.val && root.val > q.val) root = root.left;
 
         // if its in between, break, found lca
