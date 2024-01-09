35b2156a8b4296ebd3e215fcafaccc8506e3d6e2
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-01-09 02:27:06 -0500

    Time: 146 ms (65.91%), Space: 18.7 MB (26.23%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        mapper = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in mapper:
+                mapper[nums[i]] = 1
+            else:
+                mapper[nums[i]]+=1
+
+            if mapper[nums[i]] >= math.ceil(len(nums)/2):
+                p1 = nums[i]
+                break
+        return p1
