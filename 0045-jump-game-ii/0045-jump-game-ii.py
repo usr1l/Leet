b6091a4c1f5d64e44bc59b1ab884dcd1080bc9bc
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-02-04 16:29:41 -0500

    Time: 105 ms (79.71%), Space: 17.9 MB (59.35%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,49 +1,22 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # the index we want to hit
-        target = len(nums) - 1
-
-        # number of jumps so far
+        end = len(nums) - 1
         num_jumps = 0
-
-        # the current farthest range we can go
         curr_max_range = 0
+        next_endpoint = 0
 
-        # surpassing means making another jump
-        next_endpoint = 0        
+        for i in range(end):
+            curr_max_range = max(curr_max_range, i+nums[i])
 
-        # loop through the num list
-        for i in range(target):
-
-            # find current max jump point, and compare with current max range
-            curr_max_range = max(curr_max_range, i + nums[i])
-
-            # if we reach the target, add a jump and break the loop
-            if curr_max_range >= target:
-                num_jumps += 1
+            if curr_max_range >= end:
+                num_jumps+=1
                 break
-
-            # if we reach the current end point, set next endpoint to the current max range
+        
             if i == next_endpoint:
-                num_jumps += 1
+                num_jumps+=1
+                
 
-                # check if there is no path, if we reach curr max range and its equal to our current endpoint
-                if next_endpoint >= curr_max_range:
-                  return False
 
-                # print(next_endpoint, curr_max_range)
                 next_endpoint = curr_max_range
 
         return num_jumps
-
-
-            
-
-
-
-
-
-
-
-        
-        
