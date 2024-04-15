70520ebed2e4fb709810938c190fdebbcb628095
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-15 16:29:31 -0400

    Time: 70 ms (23.8%), Space: 43.8 MB (100%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -1,16 +1,15 @@
 function hIndex(citations: number[]): number {
-    // if (citations.length === 1 && citations[0] == 0) return 0;
-    // let h: number = 0;
-    // citations.sort((a, b) => b - a);
-    // for (let i:number = 0; i < citations.length; i ++) {
-    //     if (citations[i] >= i + 1) {
-    //         h = i + 1
-    //     } else {
-    //         break;
-    //     }
-    // };
+    if (citations.length === 1 && citations[0] == 0) return 0;
+    let h: number = 0;
+    citations.sort((a, b) => b - a);
+    for (let i:number = 0; i < citations.length; i ++) {
+        if (citations[i] >= i + 1) {
+            h = i + 1
+        } else {
+            break;
+        }
+    };
 
-    // return h;
+    return h;
     
-    return citations.sort((a, b) => b - a).filter((val, i) => val > i).length;
 };
