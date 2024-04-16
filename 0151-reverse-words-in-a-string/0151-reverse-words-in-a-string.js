31a9998938c216ab88612e2d0c38e6690fe8c12f
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-16 13:33:28 -0400

    Time: 49 ms (84.49%), Space: 50.4 MB (81.72%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,26 +3,11 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    // const res = [];
-    // let currWord = '';
-    // for (let i = 0; i < s.length; i++) {
-    //     // console.log(s[i] === ' ', s[i], currWord.length, currWord.length > 0)
-    //     if (s[i] === ' ' && currWord) {
-    //         // console.log(currWord);
-    //         res.unshift(currWord);
-    //         currWord = '';
-
-    //     } else if (s[i] === ' ') {
-    //         continue;
-    //     }
-    //     else if (s[i] && s[i] !== ' ') {
-    //         currWord += s[i];
-    //     };
-    // };
-
-    // currWord ? res.unshift(currWord) : null;
-
-    // return res.join(' ');
-
-    return s.split(' ').filter(word => word).reverse().join(' ');
+    s = s.trim();
+    let arr = s.split(" ");
+    let res = [];
+    for (let i = arr.length-1; i >= 0; i--) {
+        if (arr[i] !== " " && arr[i] !== "") res.push(arr[i]);
+    };
+    return res.join(" ");
 };
