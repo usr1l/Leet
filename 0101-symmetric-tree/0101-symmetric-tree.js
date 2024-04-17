14eaa0893c25b2d8039b7adfaae3c34f8c2f9a01
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-17 17:40:40 -0400

    Time: 62 ms (50.53%), Space: 44.8 MB (100%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -5,7 +5,7 @@
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
  * }
- */
+ */ 
 /**
  * @param {TreeNode} root
  * @return {boolean}
@@ -13,33 +13,14 @@
 var isSymmetric = function(root) {
     if (!root) return true;
 
-    function traverse(root, left=false, path = []) {
-        if (!root) {
-            path.push(null);
-            return '';
-        };
+    function mirrorTree(left, right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if (left.val !== right.val) return false;
 
-        path.push(root.val);
-        if (left) {
-
-            traverse(root.left, left, path);
-            traverse(root.right, left, path);
-        } else {
-            traverse(root.right, left=false, path);
-            traverse(root.left, left=false, path);
-            
-        } 
-
-        return path
+        return mirrorTree(left.left, right.right) && mirrorTree(left.right, right.left);
     };
 
-    const path1 = traverse(root.left, left=true);
-    const path2 = traverse(root.right);
-
-    if (path1.length !== path2.length) return false;
-    for (let i = 0; i < path1.length; i ++) {
-        if (path1[i] !== path2[i]) return false;
-    };
-
-    return true;
+    return mirrorTree(root.left, root.right);
+    
 };
