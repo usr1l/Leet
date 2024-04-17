6d65142b81c0b6de2be5470cca0a4c22c8608b8c
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-17 17:12:07 -0400

    Time: 56 ms (34.12%), Space: 48.8 MB (79.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,7 +12,24 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (!p && !q) return true;
-    if (!p || !q || p.val !== q.val) return false;
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
+    function traverse(root, arr=[]) {
+        if (!root) {
+            arr.push(null);
+            return ''
+        };
+        arr.push(root.val);
+        traverse(root.left, arr);
+        traverse(root.right, arr);
+        return arr;
+    };
+
+    const arr1 = traverse(p) 
+    const arr2 = traverse(q)
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+        };
+
+    return true;
 };
