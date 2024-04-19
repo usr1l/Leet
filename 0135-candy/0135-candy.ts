119de5f12b5a05e4ba1b127d9017f556e66ea8c6
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-19 02:36:50 -0400

    Time: 54 ms (98.14%), Space: 46.5 MB (100%) - LeetHub

--- 0135-candy/0135-candy.ts
+++ 0135-candy/0135-candy.ts
@@ -5,19 +5,16 @@ function candy(ratings: number[]): number {
     // increment based on need
 
     const res: number[] = new Array(ratings.length).fill(1);
-    // check twice through the array, from both ends, comparing higher or lower
     for (let i: number = 0; i < ratings.length - 1; i ++) {
         if (ratings[i + 1] > ratings[i] && res[i] >= res[i + 1]) {
-            // accounting for numbers that keep going up
             res[i + 1] += res[i];
         }
     }
     for (let i: number = ratings.length - 1; i > 0; i --) {
         if (ratings[i - 1] > ratings[i] && res[i - 1] <= res[i]) {
-            // accounting for numbers that go down
             res[i - 1] = res[i] + 1;
         }
     }
-    // console.log(res)
+    console.log(res)
     return res.reduce((accum, val) => accum += val, 0);
 };
