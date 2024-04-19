239e711e5bb3fcc89da7b84bf112d281092f24f2
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-19 03:56:54 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dict1 = {}
+        for item in magazine:
+            if item not in dict1:
+                dict1[item] = 1
+            else:
+                dict1[item] += 1
+         for item in ransomNote:
+            if item not in dict1:
+                return False
+            else:
+                dict1[item] -= 1
+
+            if dict1[item] < 0:
+                return False
+        
+        return True
