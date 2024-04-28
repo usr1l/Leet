98c84ce1fb8cc4a03c2d02f133d647c2ab284ee6
Tony Zheng <97369378+usr1l@users.noreply.github.com>
2024-04-28 02:45:16 -0400

    Time: 62 ms (24.37%), Space: 44.2 MB (100%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -12,23 +12,27 @@
  */
 var removeNthFromEnd = function(head, n) {
     if (!head) return;
-    let count = 1;
-    const pointer = new ListNode(0, head);
-    let pointer1 = pointer;
-    let pointer2 = head;
-    let endPointer = head;
 
-    while (endPointer) {
-        endPointer = endPointer.next;
-        count++;
-        
-        if (count >= n + 2) {
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
-        };
+    // use a pointer, because sometimes head can get removed
+    let pointer = new ListNode(0, head);
+    let curr = head;
 
+    // map the whole list
+    let map = new Map();
+    map[`-1`] = pointer;
+    let number = 0;
+    while (curr) {
+        map[number] = curr;
+        curr = curr.next; 
+        number++;
     };
-    pointer1.next = pointer2?.next || null;
+
+    // index of the node and the one before it
+    let index = Object.keys(map).length - n;
+    const node1 = map[`${index - 2}`];
+    const node2 = map[`${index}`];
+    console.log(node1.next, node2)
+    node1.next = node2 || null;
 
     return pointer.next;
 
