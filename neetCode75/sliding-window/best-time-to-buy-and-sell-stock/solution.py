0bab3f528ef874f4f94e9e8162aaeac1ce6375c5
usr1l <zhtony0398@gmail.com>
2025-11-21 01:05:02 -0500

    best-time-to-buy-and-sell-stock

--- neetCode75/sliding-window/best-time-to-buy-and-sell-stock/solution.py
+++ neetCode75/sliding-window/best-time-to-buy-and-sell-stock/solution.py
@@ -8,3 +8,17 @@ class Solution:
             max_profit = max(max_profit, prices[i] - lowest)
 
         return max_profit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        lowest = prices[0]
+
+        for i in range(1, len(prices)):
+            curr_price = prices[i]
+            if curr_price >= prices[i-1]:
+                max_profit = max(max_profit, prices[i]-lowest)
+            else:
+                lowest = min(lowest, prices[i])
+
+        return max_profit
