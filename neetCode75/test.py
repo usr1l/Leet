9d5a3e41c821c119da2efdc68bbabd9670ade33d
usr1l <zhengtony46@gmail.com>
2025-11-26 22:29:38 -0500

    newq

--- neetCode75/test.py
+++ neetCode75/test.py
@@ -1,18 +1,16 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-b = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3]
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-c = [11, 13, 15, 17, 19, 1, 3, 5, 7, 9]
-l = 0
-r = 9
-mid = 19
+node1 = TreeNode(1)
+node2 = TreeNode(1)
 
-c_sorted = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-c_rotated = [7, 9, 11, 13, 15, 17, 19, 1, 3, 5]
-c__rotated = [15, 17, 19, 1, 3, 5, 7, 9, 11, 13]
-c_rotated__ = [3, 5, 7, 9, 11, 13, 15, 17, 19, 1]
-c_rotated = [13, 15, 17, 19, 1, 3, 5, 7, 9, 11]
+node1.left = TreeNode(2)
+node1.right = TreeNode(3)
 
-# right side:
-# target < mid, target < right, target > mid, target > right, target > mid, target > right
-# left side:
-# target < mid, target >= left
+node2.left = TreeNode(2)
+node2.right = TreeNode(3)
+
+print(node1 == node2)  # FALSE
